35a9feb6e1635da23526ddbeee13bb2a6d94ad0f
geoiraq <george.ejaam@gmail.com>
2018-03-20 21:19:18 +1100

    updating schemas

--- question.js
+++ question.js
@@ -1,12 +1,13 @@
 
 var mongoose = require('mongoose');
-var Schema = mongoose.Schema, ObjectId = Schema.ObjectId;
+var Schema = mongoose.Schema, ObjectId = Schema.Types.ObjectId;
 
 //API
 var APISchema = new Schema({
-api_name: String
-questions: [{type: ObjectId, ref:QuestionSchema}],
-methods: [{type:ObjectId, ref:MethodSchema}]
+_id:ObjectId,
+api_name: String,
+questions: [{type: ObjectId, ref:'Question'}],
+methods: [{type:ObjectId, ref:'Method'}]
 });
 
 
@@ -17,13 +18,13 @@ question_id:String,
 creation_date:Date,
 extraction_date:Date,
 body:String,
-answers: [{type: ObjectId, ref:'AnswerSchema'}],
-bugs: [{type: ObjectId, ref:'BugSchema'}],
+answers: [{type: ObjectId, ref:'Answer'}],
+bugs: [{type: ObjectId, ref:'Bug'}],
 title:String,
 tags:[String],
 author:String,
 viewed: Number,
-api_id: [{type:ObjectId, ref:APISchema}]
+api_id: [{type:ObjectId, ref:'API'}]
 
 });
 
@@ -35,10 +36,10 @@ extraction_date:Date,
 body:String,
 accepted: Boolean,
 votes: Number,
-question_id:{type:ObjectId, ref:QuestionSchema},
-code:{type:ObjectId, ref:CodeSchema}
-bug_fix:{type:ObjectId, ref:BugFixSchema}
-api_recommendation:{type:ObjectID, ref:APIRecommendationSchema}
+question_id:{type:ObjectId, ref:'Question'},
+code:{type:ObjectId, ref:'Code'},
+bug_fix:{type:ObjectId, ref:'BugFix'},
+api_recommendation:{type:ObjectId, ref:'APIRecommendation'}
 });
 
 // Bug Fix
@@ -54,18 +55,18 @@ type:String
 //Methods
 var MethodSchema = new Schema({
 name: String,
-api_id:{type:ObjectId, ref:APISchema},
+api_id:{type:ObjectId, ref:'API'},
 description: String,
-code_example: [{type:ObjectId, ref:'CodeSchema'}],
+code_example: [{type:ObjectId, ref:'Code'}],
 //bugs : {type:ObjectId, ref:'BugSchema'},
 });
 
 // Code Examples
 var CodeSchema = new Schema({
 code_content: String,
-answer_id:{type:ObjectId, ref:AnswerSchema},
+answer_id:{type:ObjectId, ref:'Answer'},
 description: String,
-methods:[{type:ObjectId, ref:MethodSchema}],
+methods:[{type:ObjectId, ref:'Method'}],
 programming_language:String
 //code_example: {type:ObjectId, ref:'CodeSchema'},
 //bugs : {type:ObjectId, ref:'BugSchema'},
@@ -75,10 +76,19 @@ programming_language:String
 
 var BugsSchema = new Schema({
  bug_description: String,
- question: {type:ObjectId, ref:QuestionSchema}, 
+ question: {type:ObjectId, ref:'Question'}, 
 });
 
 
 
 module.exports = mongoose.model('Answer', AnswerSchema);
 module.exports = mongoose.model('Question', QuestionSchema);
+module.exports = mongoose.model('API', APISchema);
+module.exports = mongoose.model('BugFix', BugFixSchema);
+module.exports = mongoose.model('Bug', BugsSchema);
+module.exports = mongoose.model('APIRecommendation',APIRecommendationSchema);
+module.exports = mongoose.model('Method',MethodSchema);
+module.exports = mongoose.model('Code',CodeSchema);
+
+
+
